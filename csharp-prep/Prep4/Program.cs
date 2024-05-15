7e1a1b331ffc29facaea70af5d175bf03262382a
bradenh7 <bradenhall7@gmail.com>
2024-05-15 11:41:26 -0600

    Completed prep 4, Number list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,54 @@
-using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.ConstrainedExecution;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int userInput = -1;
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        int highNumber = 0;
+        int lowNumber = 100000000;
+        System.Console.WriteLine("Add numbers to a list! Type '0' when finished.");
+        
+
+        while (userInput != 0)
+        {
+            System.Console.WriteLine("Number to add:");
+            string input = Console.ReadLine();
+            userInput = int.Parse(input);
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+            else
+            {}
+
+        }
+
+
+        
+        foreach(int number in numbers)
+        {
+            sum += number;
+            if (number > highNumber)
+            {highNumber = number;}
+            if (number < lowNumber)
+            {lowNumber = number;}
+        }
+        float avg = ((float)sum) / numbers.Count;
+
+        System.Console.WriteLine($"The sum is: {sum}");
+        System.Console.WriteLine($"The average is: {avg}");
+        System.Console.WriteLine($"The Highest Number is: {highNumber}");
+        System.Console.WriteLine($"The Lowest Number is: {lowNumber}");
+        
+
+
+
     }
 }
