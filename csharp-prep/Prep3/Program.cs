17dafb62a187d3166f9ef8312a4aa4a580cca6c8
bradenh7 <bradenhall7@gmail.com>
2024-05-15 10:47:09 -0600

    Completed prep 3, guess the number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = 10000;
+
+        while (magicNumber != guess)
+        {
+            System.Console.WriteLine("Guess a number between 1 and 100: ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+
+            if (guess > magicNumber)
+            {
+                System.Console.WriteLine("You guessed too high!\n");
+            }
+            else if (guess < magicNumber)
+            {
+                System.Console.WriteLine("You guessed too low!\n");
+            }
+            else
+            {
+                System.Console.WriteLine("You guessed the number!!");
+            }
+        }
+
+        
+
     }
 }
