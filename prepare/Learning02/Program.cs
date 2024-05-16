17c8342d8a5e113e0aa5203b2098ed0957a28f5a
bradenh7 <bradenhall7@gmail.com>
2024-05-15 21:33:07 -0600

    completed the main function for the resume project

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._companyName = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2024;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._companyName = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2025;
+        job2._endYear = 2030;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Braden Hall";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+
+        resume1.Display();
     }
 }
