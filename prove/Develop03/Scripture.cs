ac3036853621180ad05be8a14f540b6d360e76d8
bradenh7 <bradenhall7@gmail.com>
2024-05-20 10:50:54 -0600

    created the files and prepared to start

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Scripture
+{
+    private string scripture = "And now, I, Moroni, would speak somewhat concerning these things; I would show unto the world that faith is things which are hoped for and not seen; wherefore, dispute not becasue ye see not, for ye reveive no witness until after the trial of your faith.";
+
+
+    public string GetScripture()
+    {
+        return scripture;
+    }
+}
