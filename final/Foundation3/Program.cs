3ce1f84da478e215d1bfc6deb7ed27ac1c29c51a
bradenh7 <bradenhall7@gmail.com>
2024-07-19 12:37:40 -0600

    all the tuff

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        // Create addresses
+        Address address1 = new Address("2447 Village Street", "Twin Falls", "ID", "USA");
+        Address address2 = new Address("1337 Elite Street", "Pocatello", "ID", "USA");
+        Address address3 = new Address("3839 Rick Avenue", "Rexburg", "ID", "USA");
+
+        // Create events
+        Event lecture = new Lecture("Local Wildlife Update", "Giving an update on all the local wildlife", new DateTime(2024, 9, 12), "10:00 AM", address1, "Dr. Doof", 100);
+        Event reception = new Reception("Elayna's Reception", "Celebrate Elayna's wedding at this reception", new DateTime(2024, 10, 20), "6:00 PM", address2, "elaynabug@gmail.com");
+        Event outdoorGathering = new OutdoorGathering("City Picnic", "A fun outdoor picnic", new DateTime(2024, 8, 15), "12:00 PM", address3, "Sunny, 72°F");
+
+        // Generate marketing messages
+        List<Event> events = new List<Event> { lecture, reception, outdoorGathering };
+
+        foreach (var eventItem in events)
+        {
+            Console.WriteLine("Standard Details:");
+            Console.WriteLine(eventItem.GetStandardDetails());
+            Console.WriteLine("\nFull Details:");
+            Console.WriteLine(eventItem.GetFullDetails());
+            Console.WriteLine("\nShort Description:");
+            Console.WriteLine(eventItem.GetShortDescription());
+            Console.WriteLine("\n---------------------------------\n");
+        }
     }
 }
---
+++ final/Foundation3/Reception.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Reception : Event
+{
+    private string rsvpEmail;
+
+    public Reception(string title, string description, DateTime date, string time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        this.rsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{GetStandardDetails()}\nType: Reception\nRSVP Email: {rsvpEmail}";
+    }
+}
